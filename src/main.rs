cc6514d00d6a527eb92856efda929b5ec6336176
newvertex <mojtaba.r127@gmail.com>
2021-10-26 23:06:55 +0330

    make some breaking changes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,71 @@
 use macroquad::prelude::*;
 
 const SIDE: usize = 3;
+const WINDOW_TITLE: &str = "Tic Tac Toe";
 const WINDOW_SIZE: i32 = 600;
-const TILE_SIZE: f32 = (WINDOW_SIZE / SIDE as i32) as f32;
-
-enum Player {
-    Computer,
-    Human,
-}
+const TILE_SIZE: i32 = WINDOW_SIZE / SIDE as i32;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Shape {
     E,  // empty
     O,  // Computer
     X,  // Player
+}
+
+enum DiagonalType {
+    Left,
+    Right,
+}
+
+enum CrossType {
+    None,
+    Row(usize),
+    Column(usize),
+    Diagonal(DiagonalType),
+}
+
+struct Tile {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+    margin: f32,
+    color: Color,
+    shape: Shape,
+}
+
+impl Tile {
+    fn new(x: f32, y: f32, w: f32, h: f32, margin: f32, color: Color) -> Self {
+        Self {
+            x: (x * w) + (x * margin),
+            y: (y * h) + (y * margin),
+            w,
+            h,
+            margin,
+            color,
+            shape: Shape::E,
+        }
+    }
+
+    fn draw(&self) {
+        draw_rectangle(self.x, self.y, self.w, self.h, self.color);
+    }
+
+    fn update(&mut self) {
+        let (x, y) = mouse_position();
+        if x >= self.x && x <= self.x + self.w && y >= self.y && y <= self.y + self.h {
+            self.color.a = 200.0;
+            if is_mouse_button_pressed(MouseButton::Left) {
+                self.on_clicked();
+            }
+        } else {
+            self.color.a = 256.0;
+        }
+    }
+
+    fn on_clicked(&mut self) {
+
+    }
 }
 
 fn draw_board(board: &[[Shape; SIDE];SIDE]) {
@@ -33,45 +85,33 @@ fn draw_board(board: &[[Shape; SIDE];SIDE]) {
     }
 }
 
-fn row_crossed(board: &[[Shape; SIDE];SIDE]) -> bool {
-    for (y, row) in board.iter().enumerate() {
-        if row[0] != Shape::E && row[0] == row[1] && row[1] == row[2] {
-            return true;
+fn check_cross(board: &[[Shape; SIDE];SIDE]) -> (CrossType, Shape) {
+    for y in 0..board.len() {
+        if board[y][0] != Shape::E && board[y][0] == board[y][1] && board[y][1] == board[y][2] {
+            return (CrossType::Row(y), board[y][0]);
         }
     }
 
-    false
-}
-
-fn col_crossed(board: &[[Shape; SIDE];SIDE]) -> bool {
-    for x in 0..SIDE {
+    for x in 0..board.len() {
         if board[0][x] != Shape::E && board[0][x] == board[1][x] && board[1][x] == board[2][x] {
-            return true;
+            return (CrossType::Column(x), board[0][x]);
         }
     }
 
-    false
-}
-
-fn diagonal_crossed(board: &[[Shape; SIDE];SIDE]) -> bool {
     if board[0][0] != Shape::E && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
-        return true;
+        return (CrossType::Diagonal(DiagonalType::Left), board[0][0]);
     }
 
     if board[0][2] != Shape::E && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
-        return true;
+        return (CrossType::Diagonal(DiagonalType::Right), board[0][2]);
     }
 
-    false
-}
-
-fn check_game_over(board: &[[Shape; SIDE];SIDE]) -> bool {
-    row_crossed(board) || col_crossed(board) || diagonal_crossed(board)
+    (CrossType::None, Shape::E)
 }
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: "Tic Tac Toe".to_string(),
+        window_title: WINDOW_TITLE.to_string(),
         window_width: WINDOW_SIZE,
         window_height: WINDOW_SIZE,
         fullscreen: false,
@@ -82,11 +122,9 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    
     let mut board: [[Shape; SIDE];SIDE] = [[Shape::E; SIDE]; SIDE];
-
-    board[2][2] = Shape::X;
-    board[0][1] = Shape::O;
+    let mut players = [Shape::X, Shape::O];
+    let mut move_count = 0;
 
     loop {
         // Update
